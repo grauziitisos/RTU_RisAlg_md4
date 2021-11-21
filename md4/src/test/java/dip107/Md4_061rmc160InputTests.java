3ed593d9d38c91f13d93d599387a68790d5e1fa9
grauziitisos <85872382+grauziitisos@users.noreply.github.com>
2021-11-21 18:40:25 +0200

    unused usings removeed

--- md4/src/test/java/dip107/Md4_061rmc160InputTests.java
+++ md4/src/test/java/dip107/Md4_061rmc160InputTests.java
@@ -8,11 +8,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
